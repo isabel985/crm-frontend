8968f434a10aa332d73947745c313fd1503df9c6
isabel985 <lee.isabel59@gmail.com>
2020-08-07 11:05:54 -0500

    removed unused hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Switch, Route, useHistory } from 'react-router-dom';
 import axios from 'axios';
 import AddName from './views/AddName';
